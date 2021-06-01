04057be677b8aaa200f0cd3d7861c21463254147
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-01 12:01:07 +0530

    👨‍💻Uploading....!

---
+++ bot.py
@@ -0,0 +1,27 @@
+from telegram import Update
+from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
+import os
+
+Token =os.environ.get("MT_BOT_TOKEN",None)
+updater = Updater( Token ,use_context = True )
+
+def start(updater,context):
+ updater.message.reply_text('''👋Hi Bro or Sis Iam Welcome Messanger bot\n\n👤Any Doubt - @Mo_Tech_YT\n🔊Bot Updates - @Mo_Tech_YT\n\n😮More Details Clcik /help Button''')
+
+def help(updater,context):
+ updater.message.reply_text("👇English👇\n\n⚕️ Add Me Your Group\n⚕️ Make me an admin\n\n👇Malayalam👇\n\n⚕️അത്യം എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ആകൂ\n⚕️ എന്നെ നിങളീടെ ഗ്രൂപ്പിൽ അഡ്മിൻ ആകൂ")
+ 
+
+def add_group(update: Update, context: CallbackContext):
+    for member in update.message.new_chat_members:
+        update.message.reply_text(f'👋Hello {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖')
+
+add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
+updater.dispatcher.add_handler(add_group_handle)
+
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(CommandHandler('help',help))
+
+updater.start_polling()
+updater.idle()
