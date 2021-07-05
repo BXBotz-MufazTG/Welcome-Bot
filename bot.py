9238ff72edd530796fff061e7327d5c73fac2ff5
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-07-05 13:17:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members, update.new_chat_title:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support {chat.full.name} Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support {chat.full_name} Thank you for Joining  ')
 
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_title, add_group)
