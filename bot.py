c8cf195491ca55e703c8b1c2976f80bb0a2ed327
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-07-05 14:10:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def help(updater,context):
  
 
 def add_group(update: Update, context: CallbackContext):
-    for member in update.message.new_chat_members, Update.message.new_chat_title:
+    for member in update.message.new_chat_members, update.message.new_chat_title:
         update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support {update.chat_name} Thank you for Joining  ')
 
 
